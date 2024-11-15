510b558693127f9792df4e1ce934438eb3c2643f
alexpark0 <park.ale@northeastern.edu>
2024-11-15 14:00:15 -0500

    Initial Commit

---
+++ Moodu/MooduApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooduApp.swift
+//  Moodu
+//
+//  Created by Alex Park on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MooduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
